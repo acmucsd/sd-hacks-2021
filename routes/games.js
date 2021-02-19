6307ee5c20c9ba729c080c2fc1f0cbc7c30d71da
Daniel Truong <ddtruong06@gmail.com>
2021-02-19 02:15:41 -0800

    Added solution code to the API

--- routes/games.js
+++ routes/games.js
@@ -15,8 +15,23 @@ const { v4: uuidv4 } = require('uuid')
 /* GET games listing. */
 router.get('/', (req, res, next) => {
   const params = req.query; 
-  // TODO: Add filtering so we only get the results we want (get games with price under $10). 
-  res.status(200).json(gamesDB);
+  if (params.id) {
+    // if we are provided an id, we should only return that game
+    res.json(gamesDB[params.id])
+    return;
+  }
+  
+  
+  // TODO: Add filtering so we only get the results we want (get games with price that are under what's provided). 
+  // If you want to get the array of just the database's values, use the following.
+  const gamesList = Object.values(gamesDB);
+  let filteredGames = gamesDB;
+  
+  if (params.price) {
+    // Fancy one-liner (map, filter, and reduce function are SUPER helpful) but not necessary
+    filteredGames = gamesList.filter((game) => game["price"] < 10);
+  }
+  res.status(200).json(filteredGames);
 });
 
 /* CREATE games listing */
@@ -24,20 +39,35 @@ router.post('/', (req, res, next) => {
   const params = req.body;
   const uuid = uuidv4();
   // TODO: Take in the data from the request, create a games object using this data, and add this to our database.
+
+  gamesDB[uuid] = {
+    "name": params.name,
+    "price": params.price,
+    "notes": params.notes
+  }
   res.status(200).send(uuid);
 });
 
 /* UPDATE games listing */
 router.put('/', (req, res, next) => {
   const params = req.body; 
+  console.log(params);
+
   // verifying that id is in the request
   if (!params.id) {
     res.status(400).send("missing id in the request")
     return;
   }
-
   // TODO: Given the ID and other fields from the request, update the corresponding item in our database.
-  res.status(200).send("completed update");
+
+  // iterate through all the keys (not id) provided, and update the values in our db
+  for (let key in params) {
+    if (key !== 'id') {
+      gamesDB[params.id][key] = params[key]
+    }
+  }
+  
+  res.status(200).json(gamesDB[params.id]);
 });
 
 /* DELETE games listing */
@@ -50,7 +80,9 @@ router.delete('/', (req, res, next) => {
   }
 
   // TODO: Given the ID of the object in our request, delete the corresponding item from our database. 
-  res.status(200).send("deleted item successfully");
+  const deletedItem = gamesDB[params.id];
+  delete gamesDB[params.id];
+  res.status(200).json(deletedItem);
 });
 
 module.exports = router;
